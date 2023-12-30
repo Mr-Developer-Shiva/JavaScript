efb389a3828ea75f1a693f361e3e1e56cd03e6c8
Mr-Developer-Shiva <sivakumar23092002@gmail.com>
2023-12-30 02:15:17 -0800

    JavaScript File created

---
+++ first.js
@@ -0,0 +1,12 @@
+console.log("Hello World");         //First javaScript code 
+let myVar = 45.5;                   //Variable Declaration
+    console.log(typeof(myVar));     //which Type
+    console.log(myVar);
+let name = "Shiva";
+    console.log(name);      //String
+let age = 21;       
+    console.log(age);       //Number
+let worked = false;
+    console.log(worked);    //Boolean
+let nothing;
+    console.log(nothing);       //Undefined
